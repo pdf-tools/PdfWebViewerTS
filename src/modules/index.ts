ac6a90cf2731da145c7ce11c6fd2195b8bde1a44
PDFToolsAG <pdfsupport@pdf-tools.com>
2019-09-19 10:58:51 +0200

    merge latest

---
+++ src/modules/index.ts
@@ -0,0 +1,7 @@
+export * from './freetext-annotation/FreetextAnnotationModule'
+export * from './highlight-annotation/HighlightAnnotationModule'
+export * from './ink-annotation/InkAnnotationModule'
+export * from './mobile-popup/MobilePopupModule'
+export * from './popup/PopupModule'
+export * from './stamp-annotation/StampAnnotationModule'
+export * from './text-annotation/TextAnnotationModule'
