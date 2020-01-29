91573d87ad1725fe277e1a4c05f0e1d17de5e987
Christoph Heer <christoph.heer@hotmail.ch>
2020-01-29 08:34:05 +0100

    history

--- src/pdf-web-viewer/views/navigation/AnnotationListItem.tsx
+++ src/pdf-web-viewer/views/navigation/AnnotationListItem.tsx
@@ -4,13 +4,12 @@ import { Annotation } from '../../../pdf-viewer-api/types'
 import { PdfItemType } from '../../../pdf-viewer-api/enums'
 import { Icon, icons } from '../../../common/Icon'
 import { classNames } from '../../../common/classNames'
-import { formatDate } from '../../../common/formatDate'
+import { formatDate } from '../../../common/Tools'
 
-const AnnotationIcon: Component<
-  { itemType: number; fill: string | null },
-  PdfWebViewerState,
-  PdfWebViewerActions
-> = ({ itemType, fill }) => (state, actions) => {
+const AnnotationIcon: Component<{ itemType: number; fill: string | null }, PdfWebViewerState, PdfWebViewerActions> = ({ itemType, fill }) => (
+  state,
+  actions,
+) => {
   const bg = fill ? fill : undefined
   switch (itemType) {
     case PdfItemType.HIGHLIGHT:
@@ -37,47 +36,127 @@ const AnnotationIcon: Component<
   }
 }
 
+const HistoryItem: Component<{ item: any }, PdfWebViewerState, PdfWebViewerActions> = ({ item }) => (state, actions) => {
+  switch (item.Type) {
+    case '/Create':
+      return (
+        <li>
+          <div>
+            <Icon icon={icons.pencil} />
+            {item.T && <span class="pwv-author">{item.T}</span>}
+            <time>{formatDate(item.D)}</time>
+          </div>
+        </li>
+      )
+    case '/Edit':
+      const params: { [key: string]: string } = {}
+      if (item.Parms) {
+        for (let i = 0; i < item.Parms.length; i += 2) {
+          const key = item.Parms[i] as string
+          const value = item.Parms[i + 1] as string
+          params[key] = value
+        }
+      }
+      console.log('----')
+      console.log(params)
+      return (
+        <li>
+          <div>
+            <Icon icon={icons.pencil} />
+            {item.T && <span class="pwv-author">{item.T}</span>}
+            <time>{formatDate(item.D)}</time>
+          </div>
+          <div class="pwv-ms-custom-history-content">
+            {params['/Subj'] && <h5>{params['/Subj']}</h5>}
+            {params['/Contents'] && <p>{params['/Contents']}</p>}
+          </div>
+        </li>
+      )
+    case '/Lock':
+      return (
+        <li>
+          <div>
+            <Icon icon={icons.lock} />
+            {item.T && <span class="pwv-author">{item.T}</span>}
+            <time>{formatDate(item.D)}</time>
+          </div>
+        </li>
+      )
+    case '/Unlock':
+      return (
+        <li>
+          <div>
+            <Icon icon={icons.unlock} />
+            {item.T && <span class="pwv-author">{item.T}</span>}
+            <time>{formatDate(item.D)}</time>
+          </div>
+        </li>
+      )
+    case '/Delete':
+      return (
+        <li>
+          <div>
+            <Icon icon={icons.delete} />
+            {item.T && <span class="pwv-author">{item.T}</span>}
+            <time>{formatDate(item.D)}</time>
+          </div>
+        </li>
+      )
+
+    default:
+      return <li>...</li>
+  }
+}
+
 let dblClickTimer: number | undefined
 
 /** @internal */
-export const AnnotationListItem: Component<
-  { annotation: Annotation; selected: boolean },
-  PdfWebViewerState,
-  PdfWebViewerActions
-> = ({ annotation, selected }) => (state, actions) => (
-  <li
-    class={classNames('pwv-annotation-navigation-item', {
-      'pwv-selected': selected,
-    })}
-    onclick={() => {
-      if (dblClickTimer) {
-        window.clearTimeout(dblClickTimer)
-        dblClickTimer = undefined
-      } else {
-        dblClickTimer = window.setTimeout(() => {
-          actions.api.goToAnnotation({ annotation, action: 'select' })
+export const AnnotationListItem: Component<{ annotation: Annotation; selected: boolean }, PdfWebViewerState, PdfWebViewerActions> = ({
+  annotation,
+  selected,
+}) => (state, actions) => {
+  console.log(annotation)
+  return (
+    <li
+      class={classNames('pwv-annotation-navigation-item', {
+        'pwv-selected': selected,
+      })}
+      onclick={() => {
+        if (dblClickTimer) {
+          window.clearTimeout(dblClickTimer)
           dblClickTimer = undefined
-        }, 250)
-      }
-    }}
-    ondblclick={() => {
-      actions.api.goToAnnotation({ annotation, action: 'edit' })
-      dblClickTimer = undefined
-    }}
-  >
-    <div>
-      <AnnotationIcon itemType={annotation.itemType} fill={annotation.color} />
-      {annotation.originalAuthor && (
-        <span class="pwv-author">{annotation.originalAuthor}</span>
+        } else {
+          dblClickTimer = window.setTimeout(() => {
+            actions.api.goToAnnotation({ annotation, action: 'select' })
+            dblClickTimer = undefined
+          }, 250)
+        }
+      }}
+      ondblclick={() => {
+        actions.api.goToAnnotation({ annotation, action: 'edit' })
+        dblClickTimer = undefined
+      }}
+    >
+      <div>
+        <AnnotationIcon itemType={annotation.itemType} fill={annotation.color} />
+        {annotation.originalAuthor && <span class="pwv-author">{annotation.originalAuthor}</span>}
+        <time>{formatDate(annotation.lastModified)}</time>
+      </div>
+      <div>
+        <h5>{annotation.subject}</h5>
+        <p>{annotation.content}</p>
+      </div>
+      {!!annotation.isLocked() && <Icon icon={icons.lock} className="pwv-locked-icon" />}
+      {annotation.custom && annotation.custom.length > 0 && (
+        <div class="pwv-ms-custom-history-list">
+          <h5>History</h5>
+          <ul>
+            {annotation.custom.map(history => (
+              <HistoryItem item={history} />
+            ))}
+          </ul>
+        </div>
       )}
-      <time>{formatDate(annotation.lastModified)}</time>
-    </div>
-    <div>
-      <h5>{annotation.subject}</h5>
-      <p>{annotation.content}</p>
-    </div>
-    {!!annotation.isLocked() && (
-      <Icon icon={icons.lock} className="pwv-locked-icon" />
-    )}
-  </li>
-)
+    </li>
+  )
+}
